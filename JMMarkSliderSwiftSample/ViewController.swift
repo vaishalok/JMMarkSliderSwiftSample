79c19748d662188677db7143ff0c75ebc5eb5b87
vaishalok <vaish.alok16@gmail.com>
2016-12-19 17:28:57 +0530

    initial commit

---
+++ JMMarkSliderSwiftSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMMarkSliderSwiftSample
+//
+//  Created by Vinsol on 19/12/16.
+//  Copyright © 2016 vaishalok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
